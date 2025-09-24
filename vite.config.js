fd58170346c06044d51ba336a287c8545a5ccb81
sadeepa <ar112734@fhss.sjp.ac.lk>
2025-09-24 19:49:21 +0530

    Update Vite config and base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ict2233-ca-02-SD187/", // this is correct for GitHub Pages
 })
+
