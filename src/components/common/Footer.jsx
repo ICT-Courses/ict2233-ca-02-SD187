6458014c2fc502d55c5bd912a8eeef98e711afa6
sadeepa <ar112734@fhss.sjp.ac.lk>
2025-09-24 18:22:01 +0530

    Updated homepage UI and content

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
       },
       {
         name: 'Email',
-        href: 'mailto:sadeepa.rathnayaka@example.com',
+        href: 'mailto:sadeesd3@gmail.com',
         icon: FaEnvelope,
       },
     ],
